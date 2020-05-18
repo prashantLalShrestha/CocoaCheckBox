50858d8b78aba02404da75527a724882bb8578cd
Prashant Shrestha <prashantlurvs@gmail.com>
2020-05-18 23:39:13 +0545

    class made open

--- CocoaCheckBox.podspec
+++ CocoaCheckBox.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name = "CocoaCheckBox"
-  spec.version = "1.0.0"
+  spec.version = "1.0.1"
   spec.summary = "CocoaCheckBox is just a simple CoreFoundationKit utility Module"
 
   spec.description = <<-DESC
--- Sources/BEMCheckBox/BEMCheckBox.swift
+++ Sources/BEMCheckBox/BEMCheckBox.swift
@@ -28,7 +28,7 @@ import UIKit
 /** Tasteful Checkbox for iOS. */
 @IBDesignable
 @objc
-public class BEMCheckBox: UIControl, CAAnimationDelegate {
+open class BEMCheckBox: UIControl, CAAnimationDelegate {
     
     @objc(BEMBoxType)
     public enum BoxType : Int {
@@ -188,7 +188,7 @@ public class BEMCheckBox: UIControl, CAAnimationDelegate {
         commonInit()
     }
 
-    @objc required init?(coder: NSCoder) {
+    @objc required public init?(coder: NSCoder) {
         super.init(coder: coder)
         commonInit()
     }
